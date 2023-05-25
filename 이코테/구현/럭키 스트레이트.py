7350ba0783b75d065385884af48aba25326cd3f0
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-05-25 10:51:54 +0900

    구현: 럭키 스트레이트

---
+++ 이코테/구현/럭키 스트레이트.py
@@ -0,0 +1,15 @@
+N = str(int(input()))
+
+length = len(N)
+leftSum = 0
+rightSum = 0
+for i in range(length // 2):
+     leftSum += int(N[i])
+
+for i in range(length // 2, length):
+     rightSum += int(N[i])
+
+if leftSum == rightSum:
+     print("LUCKY")
+else:
+     print("READY")
