2257b3daa6ec58a5e2915a929b81bd4e056cb29f
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-05-29 20:53:42 +0900

    정렬: 국영수

---
+++ 이코테/정렬/국영수.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+
+students = []
+for _ in range(N):
+    students.append(input().split())
+
+students.sort(lambda x : (-int(x[1])), int(x[2]), (-int(x[3])), x[0])
+
+for student in students:
+    print(student[0])
