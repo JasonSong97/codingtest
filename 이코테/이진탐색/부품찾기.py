72b076e5e827072b03e00519e44cfae79010a925
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-06-01 11:43:17 +0900

    이진탐색: 부품 찾기

---
+++ 이코테/이진탐색/부품찾기.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+nList = list(map(int, input().split()))
+M = int(input())
+mList = list(map(int, input().split())) 
+
+
+nList.sort() # 2 3 7 8 9
+mList.sort() # 5 7 9
+
+def binarySearch(array, target, start, end):
+    while start <= end:
+         mid = (start + end) // 2
+
+         if array[mid] == target:
+              return mid
+         elif array[mid] > target:
+              end = mid - 1
+         else:
+              start = mid + 1
+    return None
+
+for target in mList:
+     result = binarySearch(nList, target, 0, len(nList) - 1)
+     if result != None:
+          print("yes", end = " ")
+     else:
+          print("no", end = " ")
