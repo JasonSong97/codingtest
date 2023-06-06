6b7aae72dbd432e2303e8f40803804fdc136952e
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-06-06 11:26:41 +0900

    구현: 문자열 재정렬

--- 이코테/구현/문자열 재정렬.py
+++ 이코테/구현/문자열 재정렬.py
@@ -1,3 +1,4 @@
+# 1
 S = input()
 value = 0
 answer = []
@@ -11,5 +12,21 @@ for s in S:
 answer.sort()
 if value != 0:
     answer.append(str(value))
+
+print(''.join(answer))
+
+# 2
+S = input()
+
+answer = []
+value = 0
+for i in range(len(S)):
+  if 48 <= ord(S[i]) <= 57:
+    value += int(S[i])
+  else:
+    answer.append(S[i])
+
+answer.sort()
+answer += str(value)
 
 print(''.join(answer))
