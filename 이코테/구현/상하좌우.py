6a681ada1b4fb78e0ba6da4f6152bb659bc2eba2
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-01 14:16:34 +0900

    구현: 상하좌우 2번째 풀이

--- 이코테/구현/상하좌우.py
+++ 이코테/구현/상하좌우.py
@@ -1,3 +1,4 @@
+# 1
 import sys
 
 n = int(sys.stdin.readline())
@@ -21,5 +22,33 @@ for way in data:
 
      x = nx
      y = ny
+
+print(x, y)
+
+# 2
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+# 방향에 대한 정의 -> 방향 벡터 사용 (L R U D)
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+directions = ['L', 'R', 'U', 'D']
+
+x, y = 1, 1 # 현재 위치
+
+goal = list(input().split())
+
+for way in goal:
+  for i in range(4):
+    if way == directions[i]:
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      if 1 <= nx <= N and 1 <= ny <= N:
+        x = nx
+        y = ny
 
 print(x, y)
