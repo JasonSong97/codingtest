75deb177a9e05a4a7cf4a7fb290a26764a05987c
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-01 14:28:25 +0900

    구현: 시각 풀이 수정

--- 이코테/구현/시각.py
+++ 이코테/구현/시각.py
@@ -1,12 +1,14 @@
 import sys
 
-n = int(sys.stdin.readline())
-answer = 0
+input = sys.stdin.readline
 
-for hour in range(n + 1):
-    for minute in range(60):
-        for second in range(60):
-            if '3' in str(hour) + str(minute) + str(second):
-                answer += 1
+N = int(input())
+
+answer = 0
+for hour in range(N + 1):
+  for minute in range(60):
+    for second in range(60):
+      if '3' in str(hour) + str(minute) + str(second):
+        answer += 1
 
 print(answer)
