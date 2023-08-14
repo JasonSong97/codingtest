16afc04fc75888b83c15243275e51cb741b719bc
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-14 12:38:38 +0900

    DP : 개미전사

---
+++ 이코테/다이나믹프로그래밍/개미전사.py
@@ -0,0 +1,16 @@
+# 1. bottom up (반복)
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+data = list(map(int, input().split()))
+
+d = [0] * 100
+
+d[0] = data[0]
+d[1] = max(data[0], data[1])
+for i in range(2, N):
+    d[i] = max(d[i - 1], d[i - 2] + data[i])
+
+print(d[N - 1])
