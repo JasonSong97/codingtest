88a85f65c1a642854ffea0365d4ef3c91da6e609
JasonSong97 <107292103+JasonSong97@users.noreply.github.com>
2024-03-12 08:29:57 +0900

    Time: 39 ms (95.29%), Space: 16.7 MB (51.59%) - LeetHub

--- 릿코드/0383-ransom-note/0383-ransom-note.py
+++ 릿코드/0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # 1
         for i in set(ransomNote):
             if magazine.count(i) < ransomNote.count(i):
                 return False
         return True
+
+        # 2
+        # st1, st2 = Counter(ransomNote), Counter(magazine) # count each letter 
+        # if st1 & st2 == st1: # check intersection
+        #     return True
+        # return False
