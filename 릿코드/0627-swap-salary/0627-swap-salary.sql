953b9eebbed7955c1688f7775d266b2a496540a4
JasonSong97 <107292103+JasonSong97@users.noreply.github.com>
2024-03-14 22:31:04 +0900

    Time: 486 ms (46.63%), Space: 0B (100%) - LeetHub

--- 릿코드/0627-swap-salary/0627-swap-salary.sql
+++ 릿코드/0627-swap-salary/0627-swap-salary.sql
@@ -1,8 +1,6 @@
-# 1
--- update Salary set sex = if(sex = 'm', 'f', 'm');
-
-update Salary set sex =
-case sex
-    when 'm' then 'f'
-    else 'm'
-end;
+UPDATE salary
+SET
+    sex = CASE sex
+        WHEN 'm' THEN 'f'
+        ELSE 'm'
+    END;
