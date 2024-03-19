e5622abb8decc3bf7ca1429b1c5163fa8c84b928
JasonSong97 <107292103+JasonSong97@users.noreply.github.com>
2024-03-20 06:22:34 +0900

    Time: 1123 ms (21.71%), Space: 0B (100%) - LeetHub

--- 릿코드/0584-find-customer-referee/0584-find-customer-referee.sql
+++ 릿코드/0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,12 +1,2 @@
 # Write your MySQL query statement below
-select 
-    c.name 
-from 
-    Customer as c
-where
-    c.referee_id != 2
-or
-    c.referee_id is null;
-
-# using COALESCE
--- select name from Customer where coalesce(referee_id, 0) <> 2;
+select name from customer where coalesce(referee_id, 0) <> 2;
