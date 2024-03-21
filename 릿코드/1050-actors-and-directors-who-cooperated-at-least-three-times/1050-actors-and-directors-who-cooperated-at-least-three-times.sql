b502ef945b7eff9214538f04dde4a1ba6deea08c
JasonSong97 <107292103+JasonSong97@users.noreply.github.com>
2024-03-21 23:18:05 +0900

    Time: 615 ms (74.63%), Space: 0B (100%) - LeetHub

--- 릿코드/1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 릿코드/1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
-select actor_id, director_id
-from ActorDirector
-group by actor_id, director_id
-having count(*) >= 3; # row가 3개이상
+-- select actor_id, director_id from actordirector group by actor_id, director_id having count(timestamp) >= 3;
+select actor_id, director_id from (select actor_id, director_id, count(timestamp) as cooperated from actordirector group by actor_id, director_id) as tablel where cooperated >= 3;
