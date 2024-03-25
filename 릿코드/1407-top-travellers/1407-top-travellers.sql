469397406f86f3717894ed2da5333279e868a9c0
JasonSong97 <107292103+JasonSong97@users.noreply.github.com>
2024-03-25 13:49:20 +0900

    Time: 1746 ms (34.23%), Space: 0B (100%) - LeetHub

--- 릿코드/1407-top-travellers/1407-top-travellers.sql
+++ 릿코드/1407-top-travellers/1407-top-travellers.sql
@@ -1,7 +1,15 @@
 # Write your MySQL query statement below
--- 사용자의 distance의 총합으로 컬럼을 뽑아내기
-select name, sum(coalesce(distance, 0)) as travelled_distance
-from users
-left join rides on users.id = rides.user_id
-group by user_id
-order by travelled_distance desc, name asc
+-- 1
+-- select name, sum(coalesce(distance, 0)) as travelled_distance
+-- from users
+-- left join rides on users.id = rides.user_id
+-- group by user_id
+-- order by travelled_distance desc, name asc
+
+-- 2
+SELECT DISTINCT u.name, 
+IFNULL(SUM(distance) OVER (PARTITION BY user_id), 0) as travelled_distance 
+FROM Rides r 
+RIGHT JOIN Users u 
+    ON r.user_id = u.id 
+ORDER BY travelled_distance DESC, name
