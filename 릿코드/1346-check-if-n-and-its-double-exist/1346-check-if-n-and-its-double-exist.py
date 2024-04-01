fceb898c930f24db719e826c9c17a949ddc6408d
JasonSong97 <107292103+JasonSong97@users.noreply.github.com>
2024-04-01 23:08:05 +0900

    Time: 57 ms (43.38%), Space: 16.8 MB (28.29%) - LeetHub

--- 릿코드/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 릿코드/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,20 +1,11 @@
-# class Solution:
-#     def checkIfExist(self, arr: List[int]) -> bool:
-#         for i in range(len(arr)):
-#             temp1 = arr[i]
-#             multi = temp1 * 2
-#             devide = temp1 / 2
-#             if multi in arr:
-#                 return True
-#             if devide and multi in arr:
-#                 return True
-
-#         return False
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        seen = set()
-        for num in arr:
-            if num * 2 in seen or (num % 2 == 0 and num // 2 in seen):
+        for i in range(len(arr)):
+            if arr[i] / 2 in arr or arr[i] * 2 in arr:
+                if arr[i] / 2 == 0 or arr[i] * 2 == 0:
+                    if arr.count(0) >= 2:
+                        return True
+                    continue
+                
                 return True
-            seen.add(num)
         return False
