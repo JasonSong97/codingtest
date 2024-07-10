216bd595bd72ed301b891af3b6caef8277fb197e
JasonSong97 <jaegeunsong97@gmail.com>
2024-07-10 23:00:31 +0900

    [Silver I] Title: 절댓값 힙, Time: 100 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -3,11 +3,26 @@ import sys
 
 input = sys.stdin.readline
 
-pq = []
+minHeap = []
+maxHeap = []
 for _ in range(int(input())):
   x = int(input())
   if x:
-    hq.heappush(pq, (abs(x), x))
+    if x > 0:
+      hq.heappush(minHeap, x)
+    else:
+      hq.heappush(maxHeap, -x)
   else:
-    print(hq.heappop(pq)[1] if pq else 0)
-    
+    if minHeap:
+      if maxHeap:
+        if minHeap[0] < abs(-maxHeap[0]):
+          print(hq.heappop(minHeap))
+        else:
+          print(-hq.heappop(maxHeap))
+      else:
+        print(hq.heappop(minHeap))
+    else:
+      if maxHeap:
+        print(-hq.heappop(maxHeap))
+      else:
+        print(0)  
