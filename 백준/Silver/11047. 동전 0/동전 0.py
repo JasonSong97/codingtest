982d130fa4bcd57096c5a4779b436cedb5eb9c44
JasonSong97 <jaegeunsong97@gmail.com>
2024-08-28 22:36:00 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,18 +1,18 @@
+# 1초 = 1억
 import sys
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-
-coin = [int(input()) for _ in range(N)]
+N, K = map(int, input().split())
+coin = [int(input()) for _ in range(N)] # 오름차순으로 주어짐
 coin.reverse()
 
 answer = 0
-for c in coin:
-  if c <= M:
-    answer += (M // c)
-    M %= c
-  else:
-    continue
+for c in coin: # 큰 수 -> 작은 수
+    if K >= c:
+        answer += (K // c)
+        K %= c
+    else: # K < c
+        continue
 
 print(answer)
