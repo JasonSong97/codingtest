43165fa3996019ba009924f1b0d6dc475947444e
JasonSong97 <jaegeunsong97@gmail.com>
2024-10-25 11:05:23 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,24 +1,31 @@
+# 자료구조 :
+# 시간복잡도 : 1초=1억,
+# 힌트 :
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+
 answer = []
-for _ in range(int(input())):
-  stk = []
-  isVPS = True
-
-  for c in input():
-    if c == '(':
-      stk.append(c)
-    else:  # )
-      if stk:
-        stk.pop()
-      else:
+for _ in range(T):
+    temp = input().rstrip()
+    stk = []
+    isVPS = True
+    for t in temp:
+        if t == '(':
+            stk.append(t)
+        else:  # )
+            if stk:
+                stk.pop()
+            else:
+                isVPS = False
+                stk.append(t)
+    if stk:
         isVPS = False
-
-  if stk:
-    isVPS = False
-
-  answer.append(isVPS)
+    answer.append(isVPS)
 
 for a in answer:
-  if a:
-    print('YES')
-  else:
-    print('NO')
+    if a:
+        print('YES')
+    else:
+        print("NO")
