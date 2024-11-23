f93cdb567f99aafdf53040bc3f57a12b61897fbd
JasonSong97 <jaegeunsong97@gmail.com>
2024-11-23 10:40:30 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -0,0 +1,21 @@
+def solution(my_string):
+    answer = []
+    
+    temp = ''
+    for s in my_string:
+        if s == ' ':
+            if temp == '':
+                continue
+            else:
+                answer.append(temp)
+                temp = ''
+        else:
+            temp += s
+    
+    if temp != '':
+        answer.append(temp)
+            
+            
+            
+        
+    return answer
