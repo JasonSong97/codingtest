52a97d95702a28e84a2b4ae1e82f53e78af28419
JasonSong97 <jaegeunsong97@gmail.com>
2024-11-24 14:16:53 +0900

    [level 0] Title: 수 조작하기 2, Time: 18.05 ms, Memory: 14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,13 @@
+def solution(numLog):
+    answer = ''
+    for i in range(1, len(numLog)):
+        minus = numLog[i] - numLog[i - 1]
+        if minus == 1:
+            answer += 'w'
+        elif minus == -1:
+            answer += 's'
+        elif minus == 10:
+            answer += 'd'
+        elif minus == -10:
+            answer += 'a'
+    return answer
