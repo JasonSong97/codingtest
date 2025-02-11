c7156f228a1eea730195c446a73d0d2a37730628
JasonSong97 <jaegeunsong97@gmail.com>
2025-02-11 10:02:32 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 겹쳐쓰기 - 181943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.09 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 23일 10:09:29
+2025년 02월 11일 10:01:39
 
 ### 문제 설명
 
--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,11 +1,2 @@
 def solution(my_string, overwrite_string, s):
-    # answer = my_string.replace(my_string[s: s + len(overwrite_string)], overwrite_string)
-    answer = ''
-    myStringData = list(my_string)
-    overWriteString = list(overwrite_string)
-    for i in range(len(overWriteString)):
-        myStringData[s + i] = overWriteString[i]
-    
-    
-    
-    return answer.join(myStringData)
+    return my_string[: s] + overwrite_string + my_string[s + len(overwrite_string): len(my_string)]
