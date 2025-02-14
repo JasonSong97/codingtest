6f34da625953c15d1bc0f95d76a71e43d5946cd5
JasonSong97 <jaegeunsong97@gmail.com>
2025-02-14 11:19:27 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,8 +1,5 @@
 def solution(age):
     answer = ''
-    alpha = ['a', 'b', 'c' ,'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ,'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    strAge = str(age)
-    for s in strAge:
-        for i in range(len(s)):
-            answer += alpha[int(s[i])]
+    for a in str(age):
+        answer += chr(int(a) + 97)
     return answer
