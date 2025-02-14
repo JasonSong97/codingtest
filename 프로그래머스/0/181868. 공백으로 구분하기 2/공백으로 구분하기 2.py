d82359a9eab69382d1deb1755b0fd297423a949e
JasonSong97 <jaegeunsong97@gmail.com>
2025-02-14 11:43:17 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -1,21 +1,3 @@
 def solution(my_string):
-    answer = []
-    
-    temp = ''
-    for s in my_string:
-        if s == ' ':
-            if temp == '':
-                continue
-            else:
-                answer.append(temp)
-                temp = ''
-        else:
-            temp += s
-    
-    if temp != '':
-        answer.append(temp)
-            
-            
-            
-        
-    return answer
+    data = list(map(str, my_string.split()))
+    return data
