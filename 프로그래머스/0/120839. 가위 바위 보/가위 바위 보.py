2063a3d2523ff659fa7ffb590d1a6b06511eff66
JasonSong97 <jaegeunsong97@gmail.com>
2025-02-14 10:48:53 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,12 +1,11 @@
-# 가위 2 / 바위 0 / 보 5
 def solution(rsp):
     answer = ''
-    for temp in rsp:
-        if temp == '2':
+    for t in rsp:
+        if t == '2':
             answer += '0'
-        elif temp == '0':
+        elif t == '0':
             answer += '5'
         else:
             answer += '2'
-    
+            
     return answer
